b257ec18e3e45a5eca4af712fcfb7f1b39824993
ririnzein <57787339+ririnzein@users.noreply.github.com>
2019-11-25 10:26:43 +0700

    commit

---
+++ Soal Sendiri Matematical.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main ()
+{
+    int y = 50;
+    int x = 20;
+
+    int penjumlahan = y + x;
+    printf("%d\n", penjumlahan);
+
+    int perkalian = y * x;
+    printf("%d\n", perkalian);
+
+    int pembagian = y / x;
+    printf("%d\n", pembagian);
+
+    int pengurangan = y - x;
+    printf("%d\n", pengurangan);
+
+    return 0;
+
+
+}
